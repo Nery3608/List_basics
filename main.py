60eb95c9468dca2bc0a687583c9067027bf6924f
Nery Estudillo <Estudner000@warren.k12.12.in.us>
2024-11-06 10:07:04 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+color_list = ["red", "green", "blue", "yellow"]
+print(color_list[0])
+
+print(color_list[-1])
+
+num_list = [1,2,3,4,5,6,7,8,9]
+
+num_list.append(5)
+print(num_list)
+
+num_list.remove(4)
+print(num_list)
+
+print(num_list.count(4))
+print(num_list.index(9))
+fruit_list = ["apple", "banana", "cherry"]
+print(fruit_list.pop())
